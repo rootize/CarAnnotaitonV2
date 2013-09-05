1ac51b4b6d6892d4bec6cc63b02a603c943ff912
rootize <hzwzijun@gmail.com>
2013-09-05 00:49:21 -0400

    end of 4/9

---
+++ src/edu/cmu/carannotationv2/EncryptedData.java
@@ -0,0 +1,12 @@
+package edu.cmu.carannotationv2;
+
+ public class EncryptedData {
+private final static String APP_ID="";
+private final static String CLIENT_KEY="";
+private final static String USR_NAME="";
+private final static String PW="";
+private final static String CLASS_NAME="";
+
+
+
+}
