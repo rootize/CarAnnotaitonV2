073f6b47fc939b805a2f64d134ea69e5fc2b3a62
ZijunWei <hzwzijun@gmail.com>
2013-10-05 02:19:52 -0400

    working version on Mac

--- src/edu/cmu/carannotationv2/JSONdata.java
+++ src/edu/cmu/carannotationv2/JSONdata.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
-import org.apache.commons.io.filefilter.FalseFileFilter;
+//import org.apache.commons.io.filefilter.FalseFileFilter;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.parse.ParseFile;
