6e836f3ed8288821f2f649ce11689a554b119742
rootize <hzwzijun@gmail.com>
2013-12-03 19:16:04 -0500

    20131203 upload from linux, TO build up a new version

--- src/edu/cmu/carannotationv2/MMdata.java
+++ src/edu/cmu/carannotationv2/MMdata.java
@@ -2,16 +2,26 @@ package edu.cmu.carannotationv2;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
 
+//import org.apache.commons.collections.MultiMap;
+//import org.apache.commons.collections.map.MultiValueMap;
+
 public class MMdata {
 	private Context context;
 private JSONArray makeJsonArray;
 private JSONArray modelJsonArray;
+private HashMap<String, String> makeHashMap; //<Makename, MakeObjectid >
+private HashMap<String,  String>modelHashMap;// <MakeObjectId+ModelName,modelObjectID>
+private HashMap<String, List<String>>modelDisplayHashMap;
+//private HashMap<String, HashMap<String, String>> modelHashMap;//<makeobjectid,<modelname,modelobjectid>>
  public MMdata(Context context,int makefileid,int modelfileid) { 
 	 this.context=context;
 	 try {
@@ -20,16 +30,84 @@ private JSONArray modelJsonArray;
 	} catch (Exception e) {
 		// TODO: handle exception
 	}
+
+}
+ 
+ public HashMap<String, List<String>> getModelDisplayHashmap() {
 	 
+	 if (modelJsonArray!=null) {
+		 for (int i = 0; i < modelJsonArray.length(); i++) {
+			 try {
+				JSONObject singgleObject=modelJsonArray.getJSONObject(i);
+				String keyString=singgleObject.getJSONObject("Make").getString("objectId");
+				List<String> modelNameList=new ArrayList<String>();
+				if (modelDisplayHashMap.get(keyString)==null) {
+					
+					modelNameList.add(singgleObject.getString("objectId"));
+					modelDisplayHashMap.put(keyString, modelNameList);
+				}else {
+					modelNameList=modelDisplayHashMap.get(keyString);
+					modelNameList.add(singgleObject.getString(keyString));
+					modelDisplayHashMap.remove(keyString);
+					modelDisplayHashMap.put(keyString, modelNameList);
+				}
+				 
+				 
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+		}
+		 return modelDisplayHashMap;
 		
-		
+	}else {
+		return null;
+	}
 	
 }
+ public HashMap<String, String> getMakeHashMap(){
+	 
+	 if (makeJsonArray!=null) {
+		for (int i = 0; i < makeJsonArray.length(); i++) {
+			try {
+				JSONObject singleObject=makeJsonArray.getJSONObject(i);
+				makeHashMap.put(singleObject.getString("name"), singleObject.getString("objectId"));
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}	
+		}
+		return makeHashMap;
+	}else {
+		return null;
+	}
+ }
+ 
+ public HashMap<String,  String> getModelHashMap() {
+	 if (modelJsonArray!=null) {
+		 for (int j = 0; j < modelJsonArray.length(); j++) {
+			 try {
+					JSONObject singleObject=modelJsonArray.getJSONObject(j);
+					modelHashMap.put(singleObject.getJSONObject("Make").getString("objectId")+singleObject.getString("name"), singleObject.getString("objectId"));
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+		}
+		return modelHashMap;
+		 
+	}else {
+		return null;
+	}
+	
+}
+ 
+ 
+ 
  private JSONArray getJsonArrayfromRaw(int id) throws Exception{
 	 InputStream stream = context.getResources().openRawResource(id);
 	 String readString=FileOperation.convertStreamToString(stream);
-	 JSONObject tempObject=new JSONObject(readString);
 	 stream.close();
+	 JSONObject tempObject=new JSONObject(readString);
+	
 	 return turnObject2Array(tempObject);
  }
  
---
+++ src/edu/cmu/carannotationv2/modelHashKey.java
@@ -0,0 +1,34 @@
+package edu.cmu.carannotationv2;
+
+
+	public final class modelHashKey<K1, K2> {
+	    private final K1 part1;
+	    private final K2 part2;
+
+	    public modelHashKey(K1 part1, K2 part2) {
+	        this.part1 = part1;
+	        this.part2 = part2;
+	    }
+
+	    @Override public boolean equals(Object other) {
+	        if (!(other instanceof modelHashKey)) {
+	            return false;
+	        }
+	        // Can't find out the type arguments, unfortunately
+	        modelHashKey rawOther = (modelHashKey) other;
+	        // TODO: Handle nullity
+	        return part1.equals(rawOther.part1) &&
+	            part2.equals(rawOther.part2);
+	    }
+
+	    @Override public int hashCode() {
+	        // TODO: Handle nullity
+	        int hash = 23;
+	        hash = hash * 31 + part1.hashCode();
+	        hash = hash * 31 + part2.hashCode();
+	        return hash;
+	    }
+
+	    // TODO: Consider overriding toString and providing accessors.
+	}
+}
